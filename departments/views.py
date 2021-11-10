07d90424d351436c6cf575c726b5b18cd78e86c0
AngelAlmaraz <64762942+AngelAlmaraz@users.noreply.github.com>
2021-11-10 15:20:51 -0600

    commit 5

---
+++ departments/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# We return what we want the user to see when directed to this route
+
+
+#  we generate a bit of context data
+
+dpts = [
+    {'Dpt_Name': 'Ventas',
+     'Contents':'Dummy content'},
+    {'Dpt_Name': 'Recursos_Humanos',
+     'Contents':'Dummy content'},
+    {'Dpt_Name': 'Marketing',
+     'Contents':'Dummy content'}
+]
+
+def home(request):
+    context = dpts
+    return render(request, 'departments/home.html', context)
+
+def about(request):
+    return render(request, 'departments/about.html')
