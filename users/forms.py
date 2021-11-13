7a96878856654b5f6aaab3ca7aa29d8ccd892ce2
AngelAlmaraz <64762942+AngelAlmaraz@users.noreply.github.com>
2021-11-13 13:02:55 -0600

    wooooooaaahhhh

--- users/forms.py
+++ users/forms.py
@@ -6,7 +6,10 @@ from departments.models import Department
 class UserRegisterForm(UserCreationForm):
     # fields = Department.objects.all()
     email = forms.EmailField()
+    dpt_list = list(Department.objects.values_list())
+    choices = [(str(i[0]), i[1]) for i in dpt_list]
+    department = forms.MultipleChoiceField(choices=choices)
 
     class Meta:
         model = User
-        fields = ['username','email','password1','password2']
+        fields = ['username','email','department','password1','password2']
