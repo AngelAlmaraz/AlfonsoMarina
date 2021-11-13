33670314e42696840374ab73dbf2cf0511a114be
AngelAlmaraz <64762942+AngelAlmaraz@users.noreply.github.com>
2021-11-13 11:53:55 -0600

    ok

--- users/forms.py
+++ users/forms.py
@@ -1,8 +1,10 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from departments.models import Department
 
 class UserRegisterForm(UserCreationForm):
+    # fields = Department.objects.all()
     email = forms.EmailField()
 
     class Meta:
