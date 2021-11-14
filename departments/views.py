ef4366ca212fb2c2107caf664b9737c1e60b76fd
AngelAlmaraz <64762942+AngelAlmaraz@users.noreply.github.com>
2021-11-13 22:16:27 -0600

    pain relieved (for now)

--- departments/views.py
+++ departments/views.py
@@ -6,6 +6,13 @@ from django.views.generic import ListView, DetailView
 # We return what we want the user to see when directed to this route
 
 
+def home(request):
+    context = {
+        'dpts': Department.objects.all()
+    }
+    return render(request, 'departments/home.html', context)
+
+
 class DepartmentListView(ListView):
     model = Department
     template_name = 'departments/home.html'
